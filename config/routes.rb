c3fa8427742bd9aaaf10b6286204fae10de822cc
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-09-09 15:20:56 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end

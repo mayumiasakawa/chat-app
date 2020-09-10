f447c617bedcf7cbbfc1b193a5afeafed448dd11
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-09-10 10:49:43 +0900

    エラーの修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,14 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
-    @message = @room.messages.includes(:user)
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-       redirect_to room_messages_path(@room)
+      redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
       render :index
